a50759deb2222e905abc0273489003bb9c9fe9fe
Panfilo Mariano Jr <panfilo@Marias-Mac-mini.local>
2013-05-02 20:31:51 +0800

    - fixed the crash from deleting time and save - done with the web view - changed the "+" button to "add" - fixed the days of month saving before the current date

---
+++ src/MTMedicineTaker/MTWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTWebViewController.h
+//  MTMedicineTaker
+//
+//  Created by Panfilo Mariano Jr. on 5/2/13.
+//  Copyright (c) 2013 Pace Creative Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTWebViewController : UIViewController
+
+@end
---
+++ src/MTMedicineTaker/MTWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  MTWebViewController.m
+//  MTMedicineTaker
+//
+//  Created by Panfilo Mariano Jr. on 5/2/13.
+//  Copyright (c) 2013 Pace Creative Studio. All rights reserved.
+//
+
+#import "MTWebViewController.h"
+
+@interface MTWebViewController ()
+
+@end
+
+@implementation MTWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
